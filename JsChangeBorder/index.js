d9e8d6cf8f67e77dc7d1173f0f8df331e5b2afd1
Victor Muhita <muhitavictor69@gmail.com>
2023-05-01 13:52:24 +0300

    styling button

--- JsChangeBorder/index.js
+++ JsChangeBorder/index.js
@@ -1,7 +1,18 @@
 let box = document.querySelector("div"),
-    input = document.querySelector("input");
+    input = document.querySelector("input"),
+    button = document.querySelector("button"),
+    buttonS = document.querySelector("#button");
 
 input.addEventListener("input",()=>{
     box.style.borderRadius = input.value;
     box.style.background = input.value;
+})
+
+console.log(button);
+
+button.addEventListener("mouseover",()=>{
+    buttonS.style.border = "3px solid rgb(0, 115, 255)"
+})
+button.addEventListener('mouseleave',()=>{
+    buttonS.style.border = "3px white solid "
 })
